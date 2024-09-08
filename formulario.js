8f4ce31c0026c847603886938919814250a98a96
Manuela Fernandez <manu.fer.0396@gmail.com>
2024-09-08 18:46:22 -0300

    Agrega validación de fecha de nacimiento y obtiene elementos del DOM

---
+++ formulario.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const style1Button = document.getElementById("style1Button");
+  const highContrastButton = document.getElementById("highContrastButton");
+  const birthDateInput = document.getElementById("birthDate");
+  const body = document.body;
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const minAdultAgeDate = new Date(
+    today.getFullYear() - 18,
+    today.getMonth(),
+    today.getDate()
+  );
+
+  birthDateInput.max = today.toISOString().split("T")[0];
+  birthDateInput.min = "1900-01-01";
+
+  birthDateInput.addEventListener("change", function () {
+    const inputDate = new Date(this.value);
+    if (inputDate > minAdultAgeDate) {
+      alert("Debes ser mayor a 18 años");
+      this.value = "";
+    }
+  });
+});
